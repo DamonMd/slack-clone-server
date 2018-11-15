505faf112f4761521f76dbb4b0c584332ff1521a
Damon Emde <damon.emde@gmail.com>
2018-11-15 15:43:21 -0500

    small updates

--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ import Sequelize from "sequelize";
 
 const sequelize = new Sequelize("slack", "damonemde", "ositos25", {
   dialect: "postgres",
+  operatorsAliases: Sequelize.Op,
   define: {
     underscored: true
   }
